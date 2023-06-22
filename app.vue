7e3329e96c9548cda375670041681f5bcf007dd8
ekkys <ekkys99@gmail.com>
2023-06-22 08:56:52 +0700

    Testing local git

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="min-h-screen w-screen bg-slate-900 text-white">
     <h1>Nuxt Blog</h1>
+  </div>
+  <div>
     <AppAlert />
   </div>
 </template>
--- components/AppAlert.vue
+++ components/AppAlert.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h2>Ini ceritanya komponen Alert</h2>
+    <h2 class="text-white">Ini ceritanya komponen Alert</h2>
   </div>
 </template>
